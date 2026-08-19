627873f1fed28553df6687607b2582aa9f17b4d2
Kwok <153464874+Blightop@users.noreply.github.com>
2026-08-19 13:29:35 +0000

    wadadwas

---
+++ C#/program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Basics {
+
+    class Program {
+
+        static void Main(string[] args) {
+            
+
+            int i = 56;
+            bool b = SecondtestFunction(50);
+            Console.WriteLine("Hello World!" + b);
+        }
+        /// <summary>
+        /// dawfafsafafwafs fuck
+        /// </summary>
+
+        void testFunction() {
+            
+        }
+
+        static bool SecondtestFunction(int i)
+        {
+            return i < 100;
+        }
+
+
+    }
+}
